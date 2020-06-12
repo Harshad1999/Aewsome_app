702fe52648d1f4242debb8121c3af0efb9f54b22
Harshad1999 <sonawaneharshad1999@gmail.com>
2020-06-12 22:57:30 +0530

    Complete

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    title: 'Awesome App',
+    home: HomePage(),
+    theme: ThemeData(primarySwatch: Colors.cyan)));
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Awesome App"),
+        ),
+        body: Container(),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              DrawerHeader(
+                child: Text(
+                  "Hii ",
+                  style: TextStyle(color: Colors.black54),
+                ),
+                decoration: BoxDecoration(color: Colors.cyanAccent),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.edit),
+        ));
+  }
+}
